7ecfa8089a4b68ad42a4767d0d5c6aab9db6ab40
Jarvis <yashavant7@gmail.com>
2020-03-13 13:54:54 -0700

    Initial commit

---
+++ src/main/java/com/springboot/myproject/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.springboot.myproject.repository;
+
+import com.springboot.myproject.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+    User findByUserName(String userName);
+}
